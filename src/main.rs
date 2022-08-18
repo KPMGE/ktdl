e497f4e4c7116e98c6cd117effa160c750a40c7c
KPMGE <jesuskevin254@gmail.com>
2022-08-18 09:07:03 -0300

    :rocket: update program

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,27 @@
 use std::fs;
+use std::fs::OpenOptions;
+use std::io::prelude::*;
+
+const TODO_FILE: &str = "/home/kevin/TODOS.md";
 
 fn write_todo(data: &str) {
-    match std::fs::write("/home/kevin/TODOS.md", "TODO: ".to_owned() + data) {
-        Ok(_v) => println!("todo added!"),
-        Err(e) => panic!("{}", e),
+    let mut file = OpenOptions::new()
+        .write(true)
+        .append(true)
+        .open(TODO_FILE)
+        .unwrap();
+
+    if let Err(e) = writeln!(file, "#TODO {}\n", data) {
+        eprintln!("Couldn't write to file: {}", e);
     }
 }
 
 fn list_todos() {
-    match fs::read_to_string("/home/kevin/TODOS.md") {
+    match fs::read_to_string(TODO_FILE) {
         Ok(result) => {
             println!("YOUR TODOS: ");
             println!("{}", result);
-        },
+        }
         Err(e) => panic!("{}", e),
     }
 }
@@ -20,22 +29,20 @@ fn list_todos() {
 fn main() {
     let command = match std::env::args().nth(1) {
         Some(v) => v,
-        None => panic!("Usage: ktdl list | add <content>"),
+        None => {
+            eprintln!("Usage: ktdl list | add <content>");
+            std::process::exit(1);
+        } 
     };
-
-    // let args: Vec<String> = std::env::args().collect();
-
-    // println!("ARGS");
 
     match command.as_str() {
         "list" => list_todos(),
-        "add" =>  {
+        "add" => {
             match std::env::args().nth(2) {
                 Some(todo) => write_todo(todo.as_str()),
-                None => panic!("Usage: ktdl list | add <content>"),
+                None => eprintln!("Usage: ktdl list | add <content>"),
             };
-            
-        },
-        _ => panic!("Invalid usage!"),
+        }
+        _ => eprintln!("Usage: ktdl list | add <content>"),
     }
 }
